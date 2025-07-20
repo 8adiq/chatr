73994e7746a4500748c8c526717a9da3aa4066bf
unknown <abdul.latif708@yahoo.com>
2025-07-20 17:23:18 +0000

    data validation done

--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from app.database import Base
 
-
 class User(Base):
     __tablename__ = 'Users'
     id = Column(Integer,primary_key=True,index=True)
--- app/schema.py
+++ app/schema.py
@@ -1,1 +1,14 @@
- 
+from pydantic import BaseModel, EmailStr
+
+class UserBase(BaseModel):
+    username : str
+    email : EmailStr
+
+class UserCreate(UserBase):
+    password : str
+
+class UserResponse(UserBase):
+    id : int
+
+    class config:
+        orm_mode = True
