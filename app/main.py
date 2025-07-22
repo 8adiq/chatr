9d68d7a813922ed61ca15e30b91e6a99173783ba
unknown <abdul.latif708@yahoo.com>
2025-07-22 14:51:46 +0000

    deployment issue

--- app/main.py
+++ app/main.py
@@ -27,6 +27,6 @@ async def startup():
 
 app.mount("/", StaticFiles(directory="auth-app-frontend/dist", html=True), name="static")
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
