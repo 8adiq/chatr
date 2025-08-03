32441e0c33615f03f822a6bf32402257a8addfe4
unknown <abdul.latif708@yahoo.com>
2025-08-03 18:48:49 +0000

    implemented async engine in database.py

--- app/database.py
+++ app/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from sqlalchemy.ext.asyncio import create_async_engine,AsyncSession
 import os
 from dotenv import load_dotenv
 
@@ -10,8 +11,8 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL environment variable is not set.")
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread":False})
-session = sessionmaker(autoflush=False,autocommit=False,bind=engine)
+engine  = create_async_engine(DATABASE_URL,echo=True)
+Session = sessionmaker(engine, class_=AsyncSession,expire_on_commit=False)
 
 Base = declarative_base()
 
