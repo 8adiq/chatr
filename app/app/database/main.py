60d8ed97d62de0bae7616645487c0a8a7e030979
unknown <abdul.latif708@yahoo.com>
2025-11-08 14:59:07 +0000

    deployment issues

--- app/app/database/main.py
+++ app/app/database/main.py
@@ -1,32 +1,33 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
-import os
-from app.config import settings
+from app.config import get_settings
 
-
-# db setup
-DATABASE_URL = settings.database_url
-
-engine = create_engine(DATABASE_URL)
-session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+def get_engine():
+    settings = get_settings()  # get settings at runtime
+    return create_engine(settings.database_url)
+
+def get_sessionmaker():
+    return sessionmaker(autocommit=False, autoflush=False, bind=get_engine())
+
 def get_db_session():
-    """ creating a dependency for db session """
-    db: Session = session() 
+    """ Dependency for db session """
+    db: Session = get_sessionmaker()()
     try:
         yield db
     finally:
         db.close()
 
 def init_db():
-    """ checking if table exits before creating"""
+    """ Create tables if they don't exist """
     from sqlalchemy import inspect
 
+    engine = get_engine()
     inspector = inspect(engine)
+
     if not inspector.has_table("Users"):
         Base.metadata.create_all(bind=engine)
         print("Database created")
     else:
-        print(f"Database table already exists")
-
+        print("Database table already exists")
