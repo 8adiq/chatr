98936d52baa39dd7f7051fd8d4cb56e6b23efb96
unknown <abdul.latif708@yahoo.com>
2025-11-22 11:02:12 +0000

    email verification

--- app/auth/service.py
+++ app/auth/service.py
@@ -168,7 +168,7 @@ def send_verification_email(user_email,token,username):
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail=f"Error sending email :{e}")
 
-settings = get_settings()
+# settings = get_settings()
 
 # def send_verification_email(user_email: str, token: str, username: str):
 #     """Send verification email using Brevo Transactional Email API"""
